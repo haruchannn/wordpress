2dbd89662d17026ae685234d5642e6809ca57031
haruchannn <haruka.senzaki@gmail.com>
2022-11-18 12:54:59 +0000

    update finalized page.php

--- wp-content/themes/myblog/functions.php
+++ wp-content/themes/myblog/functions.php
@@ -2,3 +2,14 @@
 add_action('init', function(){
     add_theme_support('post-thumbnails');
 });
+
+function get_eyecatch_with_default()
+{
+    if(has_post_thumbnail()): 
+           $img_id= get_post_thumbnail_id();
+           $img_src=wp_get_attachment_image_src($img_id, 'large');
+        else:
+           $img_src=array(get_template_directory_uri().'/assets/img/post-bg.png'); 
+              endif;
+    return $img_src;
+}?>
--- wp-content/themes/myblog/single.php
+++ wp-content/themes/myblog/single.php
@@ -8,42 +8,37 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <?php get_template_part('includes/header') ?>
-    <?php if(have_posts()) : ?>
-    <?php while (have_posts()) : the_post();?>
-        <!-- Page Header-->
-        <?php  if(has_post_thumbnail()): 
-           $img_id= get_post_thumbnail_id();
-           $img_src=wp_get_attachment_image_src($img_id, 'large');
-        else:
-           $img_src=array(get_template_directory_uri().'/assets/img/post-bg.png'); 
-              endif;?>
-        <header class="masthead" style="background-image: url('<?php echo $img_src[0] ?>)">
-            <div class="container position-relative px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <div class="post-heading">
-                            <h1><?php the_title(); ?></h1>
-                            <span class="meta">
-                                Posted by
-                                <?php the_author(); ?>
-                                on <?php the_date(); ?>
-                            </span>
+    <?php if (have_posts()) :; ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <!-- Page Header-->
+            <?php $img_src = get_eyecatch_with_default(); ?>
+            <header class="masthead" style="background-image: url('<?php echo $img_src[0] ?>)">
+                <div class="container position-relative px-4 px-lg-5">
+                    <div class="row gx-4 gx-lg-5 justify-content-center">
+                        <div class="col-md-10 col-lg-8 col-xl-7">
+                            <div class="post-heading">
+                                <h1><?php the_title(); ?></h1>
+                                <span class="meta">
+                                    Posted by
+                                    <?php the_author(); ?>
+                                    on <?php the_date(); ?>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </header>
-        <!-- Post Content-->
-        <article class="mb-4">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                       <?php the_content(); ?>
+            </header>
+            <!-- Post Content-->
+            <article class="mb-4">
+                <div class="container px-4 px-lg-5">
+                    <div class="row gx-4 gx-lg-5 justify-content-center">
+                        <div class="col-md-10 col-lg-8 col-xl-7">
+                            <?php the_content(); ?>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </article>
-    <?php endwhile; ?>
+            </article>
+        <?php endwhile; ?>
     <?php endif; ?>
     <?php get_template_part('includes/footer') ?>
     <?php get_footer(); ?>
